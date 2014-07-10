19f2b3211b61523eaab0816f8d7519dfedca9a49
Servletparty <remi.martin20@gmail.com>
2014-07-10 17:21:05 +0200

    NA

--- app/js/directives/container.js
+++ app/js/directives/container.js
@@ -1,39 +1,40 @@
-angular.module("Container", []).directive("container", function($compile){
+var container = angular.module("Container", []);
+
+container.controller("containerCtrl", function($scope){
+    
+    $scope.item = {
+        tag: "a-tag",
+        color: "#f00"
+    };
+    console.log($scope);
+});
+
+container.directive("container", function($compile){
     return {
         restrict: "EA",
-        //replace: true,
+        replace: true,
+        templateUrl: "template/container.html",
         scope: {
-            item: "@"
+            item: "="
         },
         controller: function($scope, $element, $attrs){
-            var template = '<div id="container">';
-            var item = '';
-            if($scope.item !== undefined){
-                item = '<' + $scope.item;
-//                angular.forEach(attrs, function(key, value) {
-//                    item += ' ' + value + '="' + key + '"'
-//                });
-                item += ' ></' + $scope.item + '>';
-                
-            }
-            template += item + '</div>';
-            $element.html(template);
-            $compile($element.contents())($scope);
+            console.log($scope);
         },
         link: function(scope, element, attrs){
-            var template = '<div id="container">';
-            var item = '';
-            if(scope.item !== undefined){
-                item = '<' + scope.item;
-//                angular.forEach(attrs, function(key, value) {
-//                    item += ' ' + value + '="' + key + '"'
-//                });
-                item += ' ></' + scope.item + '>';
-                
-            }
-            template += item + '</div>';
-            element.html(template);
-            $compile(element.contents())(scope);
+            
+//            var template = '<div id="container">';
+//            var item = '';
+//            if(scope.item !== undefined){
+//                item = '<' + scope.item;
+////                angular.forEach(attrs, function(key, value) {
+////                    item += ' ' + value + '="' + key + '"'
+////                });
+//                item += ' ></' + scope.item + '>';
+//                
+//            }
+//            template += item + '</div>';
+//            element.html(template);
+//            $compile(element.contents())(scope);
         }
     };
 });
